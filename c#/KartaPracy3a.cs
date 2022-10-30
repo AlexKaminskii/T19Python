183ed14baf46c0002f1108e0586cfc1246bb4d16
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2022-10-30 10:41:17 +0100

    Create KartaPracy3a.cs

---
+++ c#/KartaPracy3a.cs
@@ -0,0 +1,101 @@
+
+// zad 1
+
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("* - | ");
+}
+
+
+// tabliczka mnożenia
+
+/*
+for (int i = 1;i < 11;i++)
+{
+    for (int j = 1; j < 11; j++)
+    {
+        Console.Write(i*j + "\t") ;
+    }
+    Console.WriteLine();
+}
+*/
+
+
+
+// gwiazdki
+
+
+
+
+///
+///
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < i + 1; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+for (int i = 0; i < n; i++)
+{
+
+
+    for (int j = 0; j < n - i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - i - 1; j++)
+    {
+        Console.Write(" ");
+    }
+
+    for (int k = n - i - 1; k < n; k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine(" ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+
+for (int i = 0; i < n; i++)
+{
+
+
+    for (int j = n - i; j < n; j++)
+    {
+        Console.Write(" ");
+    }
+
+
+    for (int k = 0; k < n - i; k++)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine("");
+}
