f54c8b56d56e7ba6fe61580feebd1b1d9e327aaa
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2022-11-05 11:14:45 +0100

    zad 6

--- c#/KartaPracy3a.cs
+++ c#/KartaPracy3a.cs
@@ -102,8 +102,31 @@ for (int i = 0; i < n; i++)
 
 
 
-//
 
+// zad 6
+
+int n = int.Parse(Console.ReadLine());
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (i + j == n - 1)
+        {
+            Console.Write("?");
+        }
+        else if (i == j) 
+        {
+            Console.Write("*");
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+    }
+    Console.WriteLine();
+}
 
 
 
