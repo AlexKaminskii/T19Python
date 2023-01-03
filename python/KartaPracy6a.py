7137229715127986cd95256f2770c878e1e324a8
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2023-01-03 22:20:27 +0100

    zad 1- 5

---
+++ python/KartaPracy6a.py
@@ -0,0 +1,68 @@
+# zad 1
+
+# x = int(input())
+# suma = 0
+# while x > 0:
+#     suma += x % 10
+#     x = x // 10
+# print(suma)
+
+
+# zad 2
+
+
+# import math
+# n = int(input())
+#
+# def pierwsza(n):
+#   if n < 2:
+#     return False
+#   elif n == 2:
+#     return True
+#   for i in range(2, int(math.sqrt(n)) + 1):
+#     if n % i == 0:
+#       return False
+#   return True
+#
+# print(pierwsza(n))
+
+
+
+# zad 3
+
+# n = int(input())
+# suma = 0
+#
+# for i in range(1, n):
+#     if i%n == 0:
+#         suma += i
+# if suma == n:
+#     print("Liczba jest doskonała")
+# else:
+#     print("Liczba nie jest doskonała")
+
+
+# zad 4
+#
+def nwd(a, b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+
+# a, b = int(input()), int(input())
+
+
+# if nwd(a, b) == 1:
+#     print("Liczby są względnie pierwsze")
+# else:
+#     print("Liczby nie są względnie pierwsze")
+
+
+# zad 5
+
+
+# n = int(input())
+# for i in range(10,20):
+#     if nwd(n, i) == 1:
+#         print(f"({n},{i})")
