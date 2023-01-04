b6ee1b50e8f4b2b1abacf394697d695abd822c92
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2023-01-04 12:31:13 +0100

    Update cezar.py

--- python/cezar.py
+++ python/cezar.py
@@ -4,3 +4,25 @@ klucz = int(input("Podaj klucz: "))
 for i in range(len(napis)):
     szyfr = szyfr + chr(65 + (ord(napis[i])-65+klucz)%26)
 print("Wiadomość: ", napis, "\nZaszyfrowana wiadomość: ", szyfr)
+
+
+# odejmowanie
+def nwd(a, b):
+    while a!=b:
+        if a > b:
+            a -= b
+        if a < b:
+            b -= a
+    return a
+#modulo
+def nwd(a, b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+#nww
+    
+
+
+
+
+
