4d06ffc1451d04015420741701afa7bf3db90dd6
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2023-01-04 07:09:30 +0100

    Skończenie KartaPracy6a.py :D

--- python/KartaPracy6a.py
+++ python/KartaPracy6a.py
@@ -12,19 +12,20 @@
 
 
 
-# n = int(input())
+
 import math
 def pierwsza(n):
   if n < 2:
     return False
   elif n == 2:
     return True
-  for i in range(2, int(math.sqrt(n)) + 1):
+  for i in range(2, n):
     if n % i == 0:
       return False
   return True
 
-# print(pierwsza(n))
+n = int(input())
+print(pierwsza(n))
 
 
 
@@ -81,18 +82,25 @@ def nwd(a, b):
 # print(f"{c}  {(a%b)//nwd(a, b)}/{b//nwd(a, b)}")
 
 # zad 8
+a, b = 0, 0
 
-# def suma_dz(n):
-#     suma = 0
-#     for i in range(1, n):
-#         if n % i == 0:
-#             suma += i
-#     return suma
-#
-# for i in range(1, 10000):
-#     a = suma_dz(i)
-#     if i == suma_dz(a) and i != a:
-#         print(i, a, "to liczby zaprzyjaźnione")
+for i in range(1, 10000):
+
+    for j in range(1, 10000):
+        if i % j == 0 and i != j:
+            a += j
+        elif j % i == 0 and i != j:
+            b += i
+    if a == i and b == j:
+        print(a,"i", b, "są zaprzyjaźnione")
+print("koniec")
+
+
+
+
+    # a = suma_dz(i)
+    # if i == suma_dz(a) and i != a:
+    #     print(i, a, "to liczby zaprzyjaźnione")
 
 
 
@@ -105,3 +113,26 @@ def nwd(a, b):
 #         for j in range(10,100):
 #             if pierwsza(j):
 #                 print(f"{i}*{j} = {i*j}")
+
+
+
+# zad 10/1
+
+
+# n = int(input())
+#
+# if pierwsza(n):
+#     if pierwsza(n+2):
+#         print(n, "jest liczbą bliźniaczą, bo ma bliźniaka", n+2)
+#     else:
+#         print(n, "nie jest liczbą bliźniaczą")
+# else:
+#     print(n, "nie jest liczbą pierwszą")
+
+
+# zad 10/2
+
+
+# for i in range(1000):
+#     if pierwsza(i) and pierwsza(i+2):
+#         print(i, "ma bliźniaka", i+2)
