a7531bdb4463b59cadd157630fc1646a584edc58
AlexKaminskii <115797336+AlexKaminskii@users.noreply.github.com>
2023-01-08 17:33:39 +0100

    ponowne robienie dla ćwiczeń 1-6

--- python/KartaPracy6.py
+++ python/KartaPracy6.py
@@ -1,275 +1,74 @@
-# zad 1
-
-# a = int(input())
-# b = int(input())
-# c = int(input())
-
+# #################################################### zad 1
+# a, b, c = map(int, input().split(" "))
 # if a - b == b - c:
-#     print("Arytmetyczny - TAK")
+#     print("arytmetyczny - TAK")
 # else:
-#     print("Arytemtyczny - NIE")
-
-# if a * b == c:
-#     print("Geometryczny - TAK")
+#     print("arytmetyczny - NIE")
+#
+# if a * c == b**2:
+#     print("geometryczny - TAK")
 # else:
-#     print("Geometryczny - NIE")
-
-# if a < b < c:
-#     print("Rosnący - TAK")
+#     print("geometryczny - NIE")
+#
+# if a<b<c:
+#     print("rosnący - TAK")
 # else:
-#     print("Rosnący - NIE")
-
-# if a > b > c:
-#     print("Malejący - TAK")
+#     print("rosnący - NIE")
+#
+# if a>b>c:
+#     print("malejący - TAK")
 # else:
-#     print("Malejący - NIE")
+#     # print("malejący - NIE")
 
-# zad 2
+# #################################################### zad 2
 
 # suma = 0
-# for i in range(100, 1000):
+# for i in range(100,1000):
 #     if i % 8 == 0 and i % 16 != 0:
 #         suma += i
 # print(suma)
 
-
-# zad 3
-
-# liczba = 0
-# for i in range(10,100):
-#     if i % 7 == 0:
-#         liczba = i
-# ilosc = 0
-# for j in range(1000, 10000):
-#     if j % liczba == 0:
-#         ilosc += 1
-# print(ilosc)
-
-
-# zad 4
-
-
+# ################################################### zad 3
 
 # suma = 0
-# for i in range(10,100):
-#     if i % 10 <= (i // 10) * 2:
+# for i in range(10, 100):
+#     if i % 7 == 0:
+#         a = i
+# for i in range(1000, 10000):
+#     if i % a == 0:
 #         suma += 1
 # print(suma)
 
+# ################################################## zad 4
 
-# zad 5
+# suma = 0
+# for i in range(10,100):
+#     a = i//10
+#     b = i%10
+#     if b >= a*2:
+#         suma += 1
+# print(suma)
 
+# ################################################# zad 5
 
 # suma = 0
 # ilosc = 0
-#
 # for i in range(100,1000):
-#     if i//100 > (1%10) + (1//10)%10:
+#     if i // 100 > (1 % 10) + (1 // 10) % 10:
 #         suma += i
 #         ilosc += 1
-# print("Suma: ", suma)
-# print("Ilość: ", ilosc)
+# print("suma:", suma, "ilość", ilosc)
 
-
-# zad 6
-
+# ############################################### zad 6
 
 # n = int(input())
 # suma = 0
-# for i in range(10, 100):
+# for i in range(10,100):
 #     if i % 19 == 0:
 #         suma += i
-#         n -= 1
+#         n-=1
 #     if n == 0:
 #         break
 #     else:
 #         continue
 # print(suma)
-
-# zad 7
-
-# n = int(input())
-# suma = 0
-#
-# for i in range(999,99,-1):
-#     if i % 37 == 0:
-#         suma += i
-#         n -= 1
-#     if n == 0:
-#         break
-#     else:
-#         continue
-# print(suma)
-
-
-# zad 8
-
-
-# n = int(input())
-# suma = 0
-# znak = 3
-# a = 2
-# for i in range(n):
-#     suma += a * znak
-#     a += 1
-#     znak = znak * -1
-# print(suma)
-
-
-# zad 9
-
-
-# n = int(input())
-# wynik = 1
-# a = 1
-# b = 0
-# znak = 1
-# for i in range(n):
-#     wynik = wynik * ((a * -2) * znak)
-#     a = (a * -2) * znak
-#     znak = znak * -1
-# print(wynik)
-
-
-# zad 10
-
-# n = int(input())
-# wynik = 0
-#
-# def silnia(n):
-#     wynik = 1
-#     while n > 0:
-#         wynik = wynik * n
-#         n -= 1
-#     return wynik
-#
-# for i in range(n):
-#     wynik = wynik + silnia(n)
-#     n -= 1
-# print(wynik)
-
-
-
-# zad 11 wspólne mianowniki 1
-
-
-
-
-# def dod_ulam(a, b, x, y):
-#     e = (nww(b, y) // b) * a
-#     f = (nww(b, y) // y) * x
-#     g = e + f
-#     return f"{g}/{nww(b,y)}"
-#
-
-# def nww(a, b):
-#     iloczyn = a * b
-#     while a != b:
-#         if a > b:
-#             a -= b
-#         if b > a:
-#             b -= a
-#     nwd = a
-#     return iloczyn // nwd
-#
-#
-# n = int(input())
-#
-# a = 1
-# b = 1
-#
-# wynik_gora = 0
-# dol_ulamka = 0
-# gora_ulamka_jeden = 0
-# gora_ulamka_dwa = 0
-#
-# for i in range(n):
-#     dol_ulamka = nww((b**2), (b+1)**2)
-#     gora_ulamka_jeden = (dol_ulamka // (b**2)) * a
-#     gora_ulamka_dwa = (dol_ulamka // (b+1)**2) * (a+2)
-#
-#     wynik_gora = gora_ulamka_jeden + gora_ulamka_dwa + wynik_gora * (dol_ulamka // nww(b, b+1))
-#     a += 2
-#     b += 1
-#
-# print(wynik_gora, "/", dol_ulamka)
-
-
-
-
-# zad 11 wspólne mianowniki 2
-
-
-
-
-# n = int(input())
-# licznik = 0
-# mianownik = 1
-# for i in range(1, n+1):
-#     mianownik = mianownik * i**2
-#
-# for i in range(1, i):
-#     licznik += mianownik * (2*i-1) // (i**2)
-# licznik +=2*n-1
-# print(f"Wynik: {licznik}/{mianownik}")
-
-
-
-# zad 11 bez wspólnych mianowników
-
-
-# n = int(input())
-# suma = 0
-# for i in range(1, n+1):
-#     suma += (2*i-1)/(i*2)
-# print(suma)
-
-
-
-# zad 12
-
-
-
-# n = int(input())
-#
-# a = 1
-# b = 1
-# suma_gora = 0
-# suma_dol = 0
-#
-# for i in range(n):
-#     suma_gora += a
-#     suma_dol += b**2
-#     a += 2
-#     b += 1
-# print(suma_gora, "/", suma_dol)
-
-
-
-
-# zad 13
-
-
-# suma = 0
-# n = int(input())
-#
-# for i in range(1, n + 1):
-#     suma = suma + (i*2)/((i**3)+2)
-# print(suma)
-
-
-
-# zad 14
-
-
-# n = int(input())
-# wynik_dol = 1
-# wynik_gora = 0
-# gora = 3
-# 
-# 
-# for i in range(1, n+1):
-#     wynik_gora = gora * (gora + 1)
-#     gora += 1
-#     wynik_dol = (wynik_dol * 2) + 1
-# print(f"{wynik_gora}/{wynik_dol} = {wynik_gora/wynik_dol}")
